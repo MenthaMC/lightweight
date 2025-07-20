8363ef36881ef25109e05af4c276a7a07d96244d
xiaoxijun <A3167717663@hotmail.com>
2025-07-20 19:18:43 +0800

    Update utils.kt

--- buildSrc/src/main/kotlin/utils.kt
+++ buildSrc/src/main/kotlin/utils.kt
@@ -15,10 +15,10 @@ fun Configuration.compatibilityAttributes(objects: ObjectFactory) {
 }
 
 fun GradlePluginDevelopmentExtension.setupPlugin(prefix: String, op: Action<PluginDeclaration>) {
-    plugins.register("paperweight-$prefix") {
-        id = "io.papermc.paperweight." + prefix
-        displayName = "paperweight $prefix"
-        tags.set(listOf("paper", "minecraft"))
+    plugins.register("lightweight-$prefix") {
+        id = "dev.menthamc.lightweight." + prefix
+        displayName = "lightweight $prefix"
+        tags.set(listOf("paper", "minecraft", "light"))
         op.execute(this)
     }
 }
